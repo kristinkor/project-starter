60b5eb226c7a06e22eec15a1787810df35ebc7a5
korzhenevskaya <kristinkor@gmail.com>
2022-11-25 02:20:09 -0500

    added pages needed

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,14 @@ import PostsListPage from "./pages/PostsListPage";
 import PostFormPage from "./pages/PostFormPage";
 import ShowPostPage from "./pages/ShowPostPage";
 import AboutUsPage from "./pages/AboutUsPage";
+
+import Account from "./pages/Account";
+import CreateAccount from "./pages/CreateAccount";
 import Login from "./pages/Login";
 import Error from "./pages/Error";
+import Edit from "./pages/Edit";
+import Feed from "./pages/Feed";
+import CreatePost from "./pages/CreatePost";
 
 import { ScrollToTop } from './components/scroll-to-top'
 
@@ -23,9 +29,17 @@ function App() {
             <Route path="/posts/new" element={<PostFormPage />} />
             <Route path="/posts/:id" element={<ShowPostPage />} />
             <Route path="/about-us" element={<AboutUsPage />} />
+            <Route path="/" element={<PostsListPage />} />
+
             <Route path="/404" element={<Error />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<PostsListPage />} />
+            <Route path="/user/:id" element={<Account />} />
+            <Route path="/create" element={<CreateAccount />} />
+            <Route path="/new-post" element={<CreatePost />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="/feed" element={<Feed />} />
+            
+
           </Routes>
         </div>
       </div>
