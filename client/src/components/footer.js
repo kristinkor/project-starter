5e6698f99c5ac0a43f751c3ab7a9aa7f8cf4539f
korzhenevskaya <kristinkor@gmail.com>
2022-11-27 14:15:56 -0500

    added login page not stled yet

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,21 +5,21 @@ export default function Footer() {
   return (
     <footer className="bg-primary-400 text-sm leading-5 tracking-normal text-white lg:bg-transparent lg:text-gray-400">
       <div className="container">
-      <div className="pb-5 row">  
-        <div className="logo-footer col">iWardrobe</div>
-        <div className="col p-0"></div> 
+      <div className="pb-5 row px-6">  
+        <div className="logo-footer col-5">iWardrobe</div>
+        <div className="col-5 p-0"></div> 
         <SocialLinks className="socials mb-6 inline-flex w-full col lg:flex p-0" />
       
       </div>
-      <div className="row">
-        <div className="col">
+      <div className="row px-6">
+        <div className="col-5">
           <h3>Address</h3>
           <p>123 Some Rd,</p>
           <p>Triple Bay 1234</p>
           <p>New York, USA</p>
           <button className="mt-4"><a href="https://goo.gl/maps/qTAbHk59LEPNHX3w6">find on Map</a></button>
         </div>
-        <div className="col">
+        <div className="col-5">
           <h3>SiteMap</h3>
           <ul>
             <li><a href="">Home</a></li>
@@ -33,16 +33,14 @@ export default function Footer() {
           <p>E: contact@iwardrobe.com</p>
         </div>
       </div>
-      <div className="mx-auto w-full max-w-6xl px-6">
-        <div className="relative flex flex-wrap py-10 lg:justify-between">
+
           {/* <NavLinks className="mb-6 inline-flex w-full flex-none justify-center lg:order-1 lg:mb-0 lg:flex lg:w-1/2 lg:justify-end" />
           <SocialLinks className="mb-6 inline-flex w-full flex-none justify-center lg:flex lg:justify-end" /> */}
-          <div className="mb-6 inline-flex w-full flex-none justify-center lg:w-1/2 lg:justify-start">
-            &copy; 2022 iWardrobe, all rights reserved
-          </div>
-        </div>
+          <p className="my-5 inline-flex w-full flex-none  lg:w-1/2 lg:justify-start">
+            &copy; 2022 iWardrobe, powered by Kristina Korzhenevskaya & Alif Rahi. All rights reserved
+          </p>
       </div>
-      </div>
+   
     </footer>
   )
 }
